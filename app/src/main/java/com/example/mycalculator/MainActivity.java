2d20ba0ab5248505af5f1459c7d360ab6f79c1e0
amirhome <amir.hosseinzadeh@flo.com.tr>
2019-04-11 16:33:27 +0300

    Evaluate

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         defineObjects();
+
+
     }
 
     private void defineObjects() {
@@ -66,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view)
     {
+
         switch (view.getId()) {
             case R.id.btn_abs:
                 monitor.append("0");
@@ -90,7 +94,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 result.setText("");
             break;
             case R.id.btn_equal:
-                result.setText("xxxxx");
+
+                result.setText( String.valueOf(evaluate(monitor.getText().toString())) );
+//                result.setText( monitor.getText().toString() );
             break;
             case R.id.btn_0:
                 monitor.append("0");
@@ -123,5 +129,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 monitor.append("9");
             break;
         }
+    }
+
+    private static String addSpaces(String exp){
+
+        //Add space padding to operands.
+        //https://regex101.com/r/sJ9gM7/73
+        exp = exp.replaceAll("(?<=[0-9()])[÷]", " / ");
+        exp = exp.replaceAll("(?<=[0-9()])[\\^]", " ^ ");
+        exp = exp.replaceAll("(?<=[0-9()])[x]", " * ");
+        exp = exp.replaceAll("(?<=[0-9()])[+]", " + ");
+        exp = exp.replaceAll("(?<=[0-9()])[-]", " - ");
+
+        //Keep replacing double spaces with single spaces until your string is properly formatted
+    /*while(exp.indexOf("  ") != -1){
+        exp = exp.replace("  ", " ");
+     }*/
+        exp = exp.replaceAll(" {2,}", " ");
+
+        return exp;
+    }
+
+    public static Double evaluate(String expr){
+
+        DecimalFormat df = new DecimalFormat("#.####");
+
+        //Format the expression properly before performing operations
+        String expression = addSpaces(expr);
+
+        try {
+            //We will evaluate using rule BDMAS, i.e. brackets, division, power, multiplication, addition and
+            //subtraction will be processed in following order
+            int indexClose = expression.indexOf(")");
+            int indexOpen = -1;
+            if (indexClose != -1) {
+                String substring = expression.substring(0, indexClose);
+                indexOpen = substring.lastIndexOf("(");
+                substring = substring.substring(indexOpen + 1).trim();
+                if(indexOpen != -1 && indexClose != -1) {
+                    Double result = evaluate(substring);
+                    expression = expression.substring(0, indexOpen).trim() + " " + result + " " + expression.substring(indexClose + 1).trim();
+                    return evaluate(expression.trim());
+                }
+            }
+
+            String operation = "";
+            if(expression.indexOf(" / ") != -1){
+                operation = "/";
+            }else if(expression.indexOf(" ^ ") != -1){
+                operation = "^";
+            } else if(expression.indexOf(" * ") != -1){
+                operation = "*";
+            } else if(expression.indexOf(" + ") != -1){
+                operation = "+";
+            } else if(expression.indexOf(" - ") != -1){ //Avoid negative numbers
+                operation = "-";
+            } else{
+                return Double.parseDouble(expression);
+            }
+
+            int index = expression.indexOf(operation);
+            if(index != -1){
+                indexOpen = expression.lastIndexOf(" ", index - 2);
+                indexOpen = (indexOpen == -1)?0:indexOpen;
+                indexClose = expression.indexOf(" ", index + 2);
+                indexClose = (indexClose == -1)?expression.length():indexClose;
+                if(indexOpen != -1 && indexClose != -1) {
+                    Double lhs = Double.parseDouble(expression.substring(indexOpen, index));
+                    Double rhs = Double.parseDouble(expression.substring(index + 2, indexClose));
+                    Double result = null;
+                    switch (operation){
+                        case "/":
+                            //Prevent divide by 0 exception.
+                            if(rhs == 0){
+                                return null;
+                            }
+                            result = lhs / rhs;
+                            break;
+                        case "^":
+                            result = Math.pow(lhs, rhs);
+                            break;
+                        case "*":
+                            result = lhs * rhs;
+                            break;
+                        case "-":
+                            result = lhs - rhs;
+                            break;
+                        case "+":
+                            result = lhs + rhs;
+                            break;
+                        default:
+                            break;
+                    }
+                    if(indexClose == expression.length()){
+                        expression = expression.substring(0, indexOpen) + " " + result + " " + expression.substring(indexClose);
+                    }else{
+                        expression = expression.substring(0, indexOpen) + " " + result + " " + expression.substring(indexClose + 1);
+                    }
+                    return Double.valueOf(df.format(evaluate(expression.trim())));
+                }
+            }
+        }catch(Exception exp){
+            exp.printStackTrace();
+        }
+        return 0.0;
     }
 }
